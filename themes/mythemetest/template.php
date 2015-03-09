9fb1a73ad88278f996b9b75273c558a94de3d753
deader <deader25@ukr.net>
2015-03-09 13:55:36 +0200

    Version-2 (X-ray)
    Создание модуля X-ray

--- themes/mythemetest/template.php
+++ themes/mythemetest/template.php
@@ -154,3 +154,20 @@ function mythemetest_field__taxonomy_term_reference($variables) {
 
   return $output;
 }
+
+
+
+/* CUSTOM HOOK*/
+
+/** 
+Х –еализуем hook_theme(). 
+*/ 
+/* function mythemetest_theme()  {  
+	return array( 
+	  'contact_site_form'  => array( 
+		'render element'  =>  'form', 
+		'path'  => drupal_get__path('theme',   'mytheme') . '/templates', 
+		'template'  =>  'contact-site-form',
+	  ),
+	); 
+} */
